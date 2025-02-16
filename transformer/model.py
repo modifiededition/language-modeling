10c12cfcf184357d412a4dd74b510b248c11c807
Ashish <ashishgy77@gmail.com>
2025-02-16 15:54:20 +0530

    add: decoder

--- transformer/model.py
+++ transformer/model.py
@@ -159,7 +159,32 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x  = layer(x, mask)
         return self.norm(x)
+    
+class DecoderBlock(nn.Module):
 
+    def __init__(self, self_attention_block: MultiHeadAttention, cross_attention_block: MultiHeadAttention, feed_forward_block:FeedForwardBlock, dropout:float):
+        super().__init__()
 
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.dropout = nn.Dropout(dropout)
+        self.residual_layers = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+    
+    def forward(self,x, encoder_output, src_mask, tgt_mask):
 
+        x = self.residual_layers[0](x, lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_layers[1](x, lambda x: self.cross_attention_block(x,encoder_output,encoder_output, src_mask))
+        x = self.residual_layers[2](x, self.feed_forward_block)
 
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.norm = LayerNormization()
+
+    def forward(self,x,encoder_output,src_mask,tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
